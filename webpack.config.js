c7082a09216ed2b9a6c9d035ac9933b9a7e03c7a
Banstein <islambahnas1@gmail.com>
2022-03-08 12:36:57 +0200

    Update files, Add HTML, CSS skeleton

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
           'css-loader',
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
