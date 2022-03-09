f71c577fbf04c21c4963e995eb685067d930b0f8
Tufahel <tufahel96@gmail.com>
2022-03-09 18:41:56 +0600

    Remove unnecessery await from index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import './style.css';
-import _ from 'lodash';
 import { callApi } from './modules/callApi.js';
 import { getApiDetails } from './modules/getApiDetails.js';
 import { frontStructure } from './modules/frontStructure.js';
 
 const load = async () => {
   const data = await callApi();
-  await getApiDetails(data);
-  await frontStructure(data);
+  getApiDetails(data);
+  frontStructure(data);
 };
 
 load();
