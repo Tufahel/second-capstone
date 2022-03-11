6b2a34e359f5866dffe452576a113f6258085ad6
Tufahel <tufahel96@gmail.com>
2022-03-11 16:25:57 +0600

    Add new Likes, Likes count, Product Count, minimal style

--- src/modules/frontStructure.js
+++ src/modules/frontStructure.js
@@ -1,18 +1,23 @@
-export const frontStructure = (data) => {
+import { update } from './likeDetails.js';
+
+export const frontStructure = async (data) => {
   const { name } = data;
   const { id } = data;
   const img = data.sprites.other['official-artwork'].front_default;
 
+  const like = await update(id);
   const gridList = document.querySelector('.grid-list');
   gridList.innerHTML += `
         <div class="grid-item">
         <img src="${img}"/>
-        <div>
-        <h3>${name}</h3>
+        <div class="header-content">
+        <h3 class="name">${name}<h3>
+        <div class="like-details">
+        <i id="btn-like" class="fa fa-regular fa-heart btn-like"></i>
+        <span id=${id}>${like}</span>
         </div>
-        <div class="btns"><button type="button" class="btn-like">Like</button>
+        </div>
         <button type="button" class="btn-cmnt">Comments</button>
-        </div>
         </div>
     `;
 };
